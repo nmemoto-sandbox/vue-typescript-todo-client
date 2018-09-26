5142d44c01019421e8bc9479bd42c053a62df376
Takafumi Umemoto <umecch@gmail.com>
2018-09-27 00:23:07 +0900

    Element を導入

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,44 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <el-container>
+    <el-header>Header</el-header>
+    <el-container>
+      <el-aside width="200px">
+        <el-row>
+          <el-menu
+            default-active="2"
+          >
+            <el-menu-item index="0">
+              <i class="el-icon-menu"></i>
+              <router-link to="/">Home</router-link>
+            </el-menu-item>
+            <el-menu-item index="1">
+              <i class="el-icon-menu"></i>
+              <router-link to="/about">About</router-link>
+            </el-menu-item>
+          </el-menu>
+        </el-row>
+      </el-aside>
+      <el-main>
+        <router-view/>
+      </el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+  .el-header {
+    background-color: #B3C0D1;
+    color: #333;
+    line-height: 60px;
   }
-}
+  
+  .el-aside {
+    background-color: #D3DCE6;
+    color: #333;
+  }
+  
+  .el-main {
+    background-color: #E9EEF3;
+    color: #333;
+  }
 </style>
